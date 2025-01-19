1c30992e75b62a1450e07ccad74b816f305b7a0c
bilali19 <46462864+bilali19@users.noreply.github.com>
2025-01-19 07:59:30 -0800

    Add files via upload

--- password.ts
+++ password.ts
@@ -8,27 +8,24 @@ function password(str: string): boolean {
     let hasDigit: boolean = false;
   
     for (let i = 0; i < str.length; i++) {
-      const charPassword: string = str[i];
+        const charPassword: string = str[i];
   
+        if (charPassword !== charPassword.toLowerCase()) {
+            hasUpperCase = true;
+        }
   
-      if (charPassword !== charPassword.toLowerCase()) {
-        hasUpperCase = true;
-      }
+        if (charPassword !== charPassword.toUpperCase()) {
+            hasLowerCase = true;
+        }
   
-      if (charPassword !== charPassword.toUpperCase()) {
-        hasLowerCase = true;
-      }
+        if (charPassword >= '0' && charPassword <= '9') {
+            hasDigit = true;
+        }
   
-      if (charPassword >= '0' && charPassword <= '9') {
-        hasDigit = true;
-      }
-  
-      if (hasUpperCase && hasLowerCase && hasDigit) {
-        return true;
-      }
+        if (hasUpperCase && hasLowerCase && hasDigit) {
+            return true;
+        }
     }
-  
-   
     return hasUpperCase && hasLowerCase && hasDigit;
   }
   
