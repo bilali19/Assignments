8e197276f031bbbf65582d714d5952b9ac7d699d
bilali19 <46462864+bilali19@users.noreply.github.com>
2025-03-10 10:27:29 -0700

    Update reformatWords.ts using switch cases

--- reformatWords.ts
+++ reformatWords.ts
@@ -3,22 +3,22 @@ function reformatWords(words: string[]): string {
   const filteredWords = words.filter(word => word !== "");
 
   // If there are no valid words, return a space
-  if (filteredWords.length === 0) {
-    return " ";
+  switch (filteredWords.length) {
+    case 0:
+      return " ";
+
+  // Handle if only one word in the array
+    case 1;
+      return filteredWords[0];
+
+  // Handle if there are only two words in the array
+    case 2:
+      return `${filteredWords[0]} and ${filteredWords[1]}`
+    default:
+      const allButLastWord = filteredWords.slice(0, -1).join(", ");
+      const lastWord = filteredWords[filteredWords.length - 1];
+      return `${allButLastWord} and ${lastWord}`;
   }
-
-  // Handle if the sentence is only one word
-  if (filteredWords.length === 1) {
-    return filteredWords[0];
-  }
-
-  // All but the last word, joined by a comma and space
-  const allButLastWord = filteredWords.slice(0, -1).join(", ");
-
-  // Last word of the sentence
-  const lastWord = filteredWords[filteredWords.length - 1];
-
-  return `${allButLastWord} and ${lastWord}`;
 }
 
 console.log(reformatWords(['ninja', 'samurai', 'ronin']));
